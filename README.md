44408ef2769d99906e49b30dd0a027d8355d444d
mogwms <mogwms@gmail.com>
2025-11-18 20:48:05 -0500

    "began writing pseudocode and testing program"

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # receipt-generator
 ReceiptGenerator
+
+# Pseudocode
+
+```
+METHOD processReceipt(item_name, quantity, unit_price)
+
+END METHOD
+
+METHOD validateInput(item_name, quantity, unit_price)
+    IF item_name == "" THEN
+        PRINT "Empty Item Name! Please Enter a Valid Item Name!"
+        RETURN
+    ELSEIF quantity <= 0 THEN
+        PRINT "Invalid Quantity! Please Enter A Quantity of 1 or Greater!"
+        RETURN
+    ELSEIF unit_price <= 0 THEN
+        PRINT "Invalid Price! Please Enter A Price Greater Than Zero!"
+        RETURN
+    ENDIF
+END METHOD
+
+START
+    DECLARE item_name : STRING
+    DECLARE quantity : DOUBLE
+    DECLARE unit_price : DOUBLE
+    DECLARE validInput : BOOLEAN
+
+    WHILE NOT validInput DO
+        INPUT item_name, quantity, unit_price
+
+        IF validateInput(item_name, quantity unit_price) THEN
+            validInput = true
+        ENDIF
+    ENDWHILE
+
+    processReceipt(item_name, quantity, unit_price)
+END
+
+```
--- ReceiptGenerator.java
+++ ReceiptGenerator.java
@@ -1,3 +1,28 @@
+
+import java.awt.Desktop;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 public class ReceiptGenerator {
-    
+    public static void main(String[] args) throws FileNotFoundException {
+        File receipt = new File("C:\\Users\\osawe\\Desktop\\Bowie State University\\Computer Science I\\Assignments\\final-programming-project\\receipt\\output.txt");
+        PrintWriter writer = new PrintWriter("C:\\Users\\osawe\\Desktop\\Bowie State University\\Computer Science I\\Assignments\\final-programming-project\\receipt\\output.txt");
+        
+        Desktop desktop = Desktop.getDesktop();
+
+        writer.print("Testing Output Using PrintWriter");
+
+        int boob = 3;
+
+        try {
+            desktop.open(receipt);
+            System.out.println("Opening File...");
+        } catch (IOException e) {
+            System.err.println("Cannot open filee");
+        }
+
+        writer.close();   
+    }
 }
