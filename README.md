e294523ea810db300d65ffb2c98df53254042d77
mogwms <mogwms@gmail.com>
2025-11-23 19:58:22 -0500

    "upd"

--- README.md
+++ README.md
@@ -7,82 +7,11 @@ ReceiptGenerator
 DECLARE tax = 6 AS DOUBLE
 
 METHOD processReceipt(item_name, quantity, unit_price, useDiscount)
-    DECLARE subtotal AS DOUBLE
-    DECLARE total AS DOUBLE
-
-    SET tax TO tax / 100
-    SET subtotal TO unit_price * quantity
-    SET total TO ROUND(subtotal * tax + subtotal) 
-END METHOD
-
-METHOD validateInput(item_name, quantity, unit_price)
-    IF item_name == "" THEN
-        PRINT "Empty Item Name! Please Enter a Valid Item Name!"
-        RETURN FALSE
-    ELSEIF quantity <= 0 THEN
-        PRINT "Invalid Quantity! Please Enter A Quantity of 1 or Greater!"
-        RETURN FALSE
-    ELSEIF unit_price <= 0 THEN
-        PRINT "Invalid Price! Please Enter A Price Greater Than Zero!"
-        RETURN FALSE
-    ENDIF
-    RETURN TRUE
+    
 END METHOD
 
 START
-    DECLARE item_name AS STRING
-    
-    DECLARE quantity AS DOUBLE
-    DECLARE unit_price AS DOUBLE
-
-    DECLARE validInput AS BOOLEAN
-    DECLARE usingDiscount AS BOOLEAN
-
-    DECLARE section AS INTEGER
-    SET section TO 1
-
-    WHILE NOT validInput DO
-        SWITCH(section)
-            CASE 1:
-                PRINT "Input Item Name"
-                INPUT item_name
-                PRINT "Input Item Quantity"
-                INPUT quantity
-                PRINT "Input Unit Price Of Item"
-                INPUT unit_price
-                
-                IF validateInput(item_name, quantity unit_price) THEN
-                    SET section TO 2
-                ENDIF
-            BREAK
-            CASE 2:
-            IF quantity > 10 THEN
-                PRINT "Discount Available! Would you like to apply the 5% off discount for buying 10 items or more? (Y/N)"
-
-                DECLARE answer AS STRING
-                INPUT answer
-
-                IF answer == "Y" THEN
-                    SET usingDiscount TO TRUE
-                ELSEIF answer == "N" THEN
-                    SET usingDiscount TO FALSE
-                ELSE
-                    PRINT "Invalid Input! Please Enter 'Y' or 'N'!"
-                    CONTINUE
-                ENDIF
-
-                SET section TO 3
-            ENDIF
-            
-            BREAK
-            CASE 3:
-                SET validInput TO TRUE
-                BREAK
-            DEFAULT:
-                BREAK
-    ENDWHILE
-    
-    processReceipt(item_name, quantity, unit_price, usingDiscount)
+   
 END
 
 ```
--- ReceiptGenerator.java
+++ ReceiptGenerator.java
@@ -7,25 +7,38 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Scanner;
 
-
 public class ReceiptGenerator {
-    static final double tax = 6;
-    
-    public static void createReceipt(String itemName, int quantity, double unitPrice) {
-        File output = new File("output.txt");
 
-        double ftax = tax / 100;
-        double subtotal = unitPrice * quantity;
-        double total = subtotal + subtotal * ftax;
+    static final double tax = 6; // declare constant for tax - 6% tax 
+    static final double discount = 5; // declare constant for discount - 5% off
 
-        try (PrintWriter writer = new PrintWriter("output.txt")) {
-            Desktop desktop = Desktop.getDesktop();
-            writer.print(itemName);
-            writer.print("\n" + subtotal);
-            writer.print("\n" + total);
-            writer.close();
-            try {
-                desktop.open(output);
+    public static void createReceipt(String itemName, int quantity, double unitPrice, boolean useDiscounts) { // declare new method createReceipt with parameters itemName, quantity, and unitPrice
+        File receipt = new File("output.txt"); // create file to write to for receipt
+
+        double ftax = tax / 100; // calculate tax
+        double subtotal = unitPrice * quantity; // calculate subtotal
+
+        if (useDiscounts) {
+           // TODO: Add discounts
+        }
+        
+        double total = subtotal + subtotal * ftax; // add tax to subtotal and calculate total price
+
+        try (PrintWriter writer = new PrintWriter("output.txt")) { // create try-catch to write to file using PrintWriter
+            writer.println("========== RECEIPT =========="); // write receipt title and line break for formatting
+            writer.println(); // make space
+            writer.println("Item: " + itemName); // write item name to file
+            writer.println("Quantity: " + quantity); // write item quantity to file
+            writer.println(); // make space
+            writer.println("============================="); // make line break for formatting
+            writer.println(); // make space
+            writer.println("Subtotal: " + subtotal); // write subtotal to file
+            writer.println("Total: " + total); // write total to file
+            writer.close(); // close writer
+
+            Desktop desktop = Desktop.getDesktop(); // get user's desktop to open receipt file
+            try { // create try-catch to open receipt file
+                desktop.open(receipt); // open receipt file
                 System.out.println("Opening File...");
             } catch (IOException e) {
                 System.err.println("Cannot open file");
@@ -34,21 +47,39 @@ public class ReceiptGenerator {
             System.err.println("Error - Cannot Write File");
         }
     }
-    public static void main(String[] args) {
-        String itemName;
-        int quantity;
-        double unitPrice;
 
-        try (Scanner reader = new Scanner(new FileReader("receipt.txt"))) {
-            itemName = reader.nextLine();
-            quantity = reader.nextInt();
-            unitPrice = reader.nextDouble();
-            
-            createReceipt(itemName, quantity, unitPrice);
+    public static void main(String[] args) { // declare main method
+        String itemName = ""; // declare String itemName
+        int quantity = 0; // declare String quantity
+        double unitPrice = 0; // declare double UnitPrice
+        
+        boolean validInput = false;
+        boolean useDiscounts = false;
 
-            reader.close();
-        } catch (Exception ex) {
-            System.err.println("Error - Cannot Read File");
+        while (!validInput) {
+            try (Scanner reader = new Scanner(new FileReader("input.txt"))) { // create try-catch to read information from file
+                itemName = reader.nextLine(); // set itemName to first line of file
+                quantity = reader.nextInt(); // set quantity to second line of file
+                unitPrice = reader.nextDouble(); // set unitPrice to third line of file
+                useDiscounts = reader.nextBoolean();
+                if ("".equals(itemName)) {
+                    System.err.println("Invalid Item Name");
+                    break;
+                } else if (quantity <= 0) {
+                    System.err.println("Invalid Quantity");
+                    break;
+                } else if (unitPrice <= 0) {
+                    System.err.println("Invalid Unit Price");
+                    break;
+                }
+                validInput = true;
+                reader.close(); // close reader
+
+            } catch (Exception ex) {
+                System.err.println("Error - Cannot Read File");
+            }
         }
+
+        createReceipt(itemName, quantity, unitPrice, useDiscounts); // call createReceipt method and pass arguments itemName, quantity, and unitPrice
     }
 }
